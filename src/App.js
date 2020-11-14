39a9fedfb8e059d89083965ceac20682006b455f
Wallgau <maslah.olfa@gmail.com>
2020-11-14 12:37:25 +0100

    display movies list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { movies$ } from './movies';
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    movies$.then(res => {
+      return setMovieList(res);
+      
+})
+  })
+  useEffect(() => {
+    movies$.then(res => {
+      const categoryList = res.map(movie => {return movie.category})
+      return setCategories(categoryList)
+    })
+  }, [movieList.length])
+  console.log('movieList',movieList)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      <label for="category">Choose a category:</label>
+<select name="category" id="category">
+      { categories.length ? categories.map(category => {
+        return (
+  <option value={ category }>{category}</option>
+  )
+}):(<p>No categories available</p>)}
+</select>
+      <ul>
+
+        { movieList.length > 1 ? movieList.map((movie) => {
+          return (
+          
+            <li><div><h3>{ movie.category }</h3><h2>{ movie.title }</h2><p><span>likes:{movie.likes}</span><span>likes:{movie.dislikes}</span></p></div></li>
+        )
+        }) : (
+            <p>Please wait the movies to load</p>
+          )}
+      </ul>
     </div>
-  );
+    );
+  
 }
 
 export default App;
